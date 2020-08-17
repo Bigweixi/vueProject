31688d64328d23dce99ef73c1e60fbd7d7bf921d
彭人炜 <q1925186789@gmail.com>
2020-08-17 18:30:42 +0800

    尝试修复bug

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 
 import {
   Swipe,
@@ -56,5 +55,4 @@ Vue.use(GoodsActionIcon);
 new Vue({
   render: h => h(App),
   router,
-  store,
 }).$mount('#app')
